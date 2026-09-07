c8385d487ae48e43704c266db8540257f3345d4d
NitinRohilla05 <nitinrohilla875@gmial.com>
2026-09-07 22:29:51 +0530

    Configure GitHub Pages deployment

--- LogiFlow/client/next.config.ts
+++ LogiFlow/client/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+
+  basePath: "/LogiFlow",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
